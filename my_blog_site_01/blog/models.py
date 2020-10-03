a50471faccf6ac691f1f1a48cdb82baa230daca8
Subhajit <mandalsubhajit330@gmail.com>
2020-10-03 21:29:59 +0530

    Add Comment Model

--- my_blog_site_01/blog/models.py
+++ my_blog_site_01/blog/models.py
@@ -22,3 +22,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
